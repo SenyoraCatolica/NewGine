6be7bddb7fd459664d94fdf40cd0bf05ea9d4459
mgirones <miquel_giru_96@hotmail.com>
2018-11-09 17:15:14 +0100

    defining the timer for the game

--- NewGine/Timer.h
+++ NewGine/Timer.h
@@ -14,6 +14,12 @@ public:
 	void Start();
 	void Stop();
 
+	void PlayGame();
+	void PauseGame();
+	void StopGame();
+
+	void UpdateTime();
+
 	Uint32 Read();
 	float ReadSec()const;
 
@@ -22,6 +28,21 @@ private:
 	bool	running;
 	Uint32	started_at;
 	Uint32	stopped_at;
+
+
+	//Game states
+	bool is_play = false;
+	bool is_pause = false;
+	bool is_stop = true;
+
+	//real time
+	unsigned long real_time = 0;
+
+	//game time
+	unsigned long game_time = 0;
+	unsigned long game_played_time = 0;
+	unsigned long game_paused_time = 0;
+
 };
 
 #endif //__TIMER_H__
