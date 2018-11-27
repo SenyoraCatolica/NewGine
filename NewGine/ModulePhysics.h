2da720bdced9d21aa75270de13e1265939eb1bee
mgirones <miquel_giru_96@hotmail.com>
2018-11-27 13:48:38 +0100

    Updated bullet to 2017 version

--- NewGine/ModulePhysics.h
+++ NewGine/ModulePhysics.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "Module.h"
+#include "Globals.h"
+#include "Bullet/include/btBulletDynamicsCommon.h"
+
+#define GRAVITY btVector3(0.0f, -10.0f, 0.0f) 
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate(float dt);
+	update_status Update(float dt);
+	update_status PostUpdate(float dt);
+	bool CleanUp();
+};
